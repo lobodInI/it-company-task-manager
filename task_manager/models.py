097bfa11118f32d5f32a3680b8454984fdb7accd
serhii_loboda <serhiy.loboda@gmail.com>
2023-11-12 01:41:05 +0200

    add def get_absolute_url for worker model

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class Position(models.Model):
@@ -26,6 +27,9 @@ class Worker(AbstractUser):
 
     def __str__(self) -> str:
         return f"{self.position} : {self.first_name} {self.last_name}"
+
+    def get_absolute_url(self):
+        return reverse("task_manager:worker-detail", kwargs={"pk": self.pk})
 
 
 class TaskType(models.Model):
