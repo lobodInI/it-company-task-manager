0fdb75669029f8ed74b13f34631dbb71f5bbac44
serhii_loboda <serhiy.loboda@gmail.com>
2023-11-14 20:39:07 +0200

    fix model Worker for creating superuser and newly created migration.

--- task_manager/models.py
+++ task_manager/models.py
@@ -16,7 +16,11 @@ class Position(models.Model):
 
 class Worker(AbstractUser):
     position = models.ForeignKey(
-        to=Position, on_delete=models.CASCADE, related_name="workers"
+        to=Position,
+        on_delete=models.CASCADE,
+        related_name="workers",
+        null=True,
+        blank=True
     )
 
     class Meta:
