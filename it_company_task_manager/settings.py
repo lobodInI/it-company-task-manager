cc37be6e7fc7b97e9d2b75fdded87c9a6dc6da2d
serhii_loboda <serhiy.loboda@gmail.com>
2023-11-17 17:35:08 +0200

    add it-company-task-manager-dgpo.onrender.com to ALLOWED_HOST

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "it-company-task-manager-dgpo.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
